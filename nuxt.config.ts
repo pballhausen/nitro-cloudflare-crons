029b9f4724cf2243dda05649c6f4909c4dc7c4e4
Philipp Ballhausen <pballhausen@users.noreply.github.com>
2025-01-17 10:57:38 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
 export default defineNuxtConfig({
+  modules: ['@nuxthub/core'],
+  hub: {
+    // NuxtHub options
+  },
   nitro: {
     srcDir: "server",
     experimental: {
-      tasks: true
+      tasks: true,
+      openAPI: true,
     },
     scheduledTasks: {
       '* * * * *': ['my-task']
     },
     preset: 'cloudflare-module'
+  },
+  hub: {
+    database: false,
+    blob: false,
+    kv: false,
   }
 });
